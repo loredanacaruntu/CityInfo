4f484879d966f51849fedd38105208b43808d762
arobscaruntul <loredana.caruntu@arobs.com>
2018-07-10 14:33:39 +0300

    upload project

---
+++ CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    { 
+        //[HttpGet("api/cities")]
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new { id=1, Name="New York City" },
+                new { id=2, Name="Antwerp" }
+            });
+
+            //return new JsonResult(CitiesDataStore.Current.Cities);
+
+            //var temp = new JsonResult(CitiesDataStore.Current.Cities);
+            //temp.StatusCode = 200;
+            //return temp;
+        }
+
+        [HttpGet("api/cities/{id}")]
+        public JsonResult GetCity(int id)
+        {
+            return new JsonResult(
+                CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id));
+
+        }
+    }
+}
